af55d8153dfbcca9905af6b9c601afa48b06fba7
Untitled <john.d.reddock@gmail.com>
2018-05-16 16:09:14 -0400

    added api key from openweathermap.org

---
+++ src/actions/index.js
@@ -0,0 +1,2 @@
+const API_KEY='9321fbbfe33bcbf1f0f682757b9890a5';
+
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -9,14 +9,17 @@ export default class SearchBar extends Component {
         this.onInputChange = this.onInputChange.bind(this);
     }
 
-    onInputChange(event) {
-        console.log(event.target.value);
+    onInputChange(event) {        
         this.setState({ term: event.target.value });
     }
 
+onFormSubmit(event) {
+    event.preventDefault();
+}
+
     render() {
         return (
-            <form className='input-group'>
+            <form onSubmit={this.onFormSubmit} className='input-group'>
               <input 
               placeholder='Get a 5 day forecast in your favorite cities'
               className='form-control'
