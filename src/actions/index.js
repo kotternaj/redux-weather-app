e06508e59012dc9078a82e9c09334a757abcd980
Untitled <john.d.reddock@gmail.com>
2018-05-18 14:39:23 -0400

    add sparkline chart for temps

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const API_KEY='9321fbbfe33bcbf1f0f682757b9890a5';
 const ROOT_URL= `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
@@ -8,8 +8,6 @@ export const FETCH_WEATHER = 'FETCH_WEATHER'
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
-
-  console.log('Request:', request);
 
    return {
        type: FETCH_WEATHER, 
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
     
     renderWeather(cityData) {
         const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        
         return (
             <tr key={name}>
               <td>{name}</td>
+              <td>
+                  <Sparklines height={120} width={180} data={temps}>
+                    <SparklinesLine color='red' />
+                  </Sparklines>
+              </td>
             </tr>
-        )
-
+        );
     }
 
     render() {
@@ -19,7 +26,7 @@ class WeatherList extends Component {
              <thead>
                 <tr>
                   <th>City</th>
-                  <th>Temperature</th>
+                  <th>Temperature</th>{}
                   <th>Pressure</th>
                   <th>Humidity</th>
                 </tr>
